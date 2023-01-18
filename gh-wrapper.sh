bb5b4928357d5d202857bd93f5f5c706917c4ba3
lbrealdev <lbrealdeveloper@gmail.com>
2023-01-18 22:05:43 +0100

    feat: comment functions

--- gh-wrapper.sh
+++ gh-wrapper.sh
@@ -14,10 +14,12 @@ function gh_graphql_owner() {
   )
 }
 
+# Setup gh cli as a credential helper.
 function gh_setup_git() {
   gh auth setup-git
 }
 
+# Clone repository.
 function gh_clone_repository() {
   REPOSITORY="$1"
   gh_graphql_owner
